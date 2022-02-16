a2ad8002a28d882f88ebb69bdfc9aa1a6ce3e738
whiz-dev <whiz123@hanmail.net>
2022-02-16 14:48:25 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const Koa = new require('koa');
+
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
